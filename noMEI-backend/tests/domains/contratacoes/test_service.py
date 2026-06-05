32e2371b5c0360ac7b6b940e2ac19dbf03624a26
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2026-06-05 12:03:20 -0300

    test(contratacoes): finaliza os testes no service

--- noMEI-backend/tests/domains/contratacoes/test_service.py
+++ noMEI-backend/tests/domains/contratacoes/test_service.py
@@ -4,6 +4,7 @@ from unittest.mock import AsyncMock, patch
 from app.domain.contratacoes.service import ContratacaoService
 from app.domain.contratacoes.schemas import ContratacaoResponse
 
+
 @pytest.mark.asyncio
 @patch("app.domain.contratacoes.repository.ContratacaoRepository.find_all")
 async def test_listar_contratacoes_paginacao_matematica(mock_find_all):
@@ -27,3 +28,39 @@ async def test_listar_contratacoes_paginacao_matematica(mock_find_all):
         busca=None,
         cnae=None
     )
+
+
+@pytest.mark.asyncio
+@patch("app.domain.contratacoes.service.ContratacaoRepository.find_by_id")
+async def test_obter_contratacao_existente(mock_find_by_id):
+
+    licitacao_falsa = {"numeroControlePNCP": "12345", "objeto": "Computadores"}
+    mock_find_by_id.return_value = licitacao_falsa
+    
+    service = ContratacaoService()
+    
+    resultado = await service.obter_contratacao("12345")
+    
+    assert resultado == licitacao_falsa
+    
+    mock_find_by_id.assert_called_once_with("12345")
+
+
+@pytest.mark.asyncio
+@patch("app.domain.contratacoes.service.ContratacaoRepository.get_estatisticas")
+async def test_obter_estatisticas_corretamente(mock_get_estatisticas):
+
+    dados_estatisticos_falsos = {
+        "totalContratacoesAbertas": 100,
+        "totalCompativeisMEI": 50
+    }
+    mock_get_estatisticas.return_value = dados_estatisticos_falsos
+    
+    service = ContratacaoService()
+    
+    resultado = await service.obter_estatisticas()
+
+    assert resultado["totalContratacoesAbertas"] == 100
+    assert resultado["totalCompativeisMEI"] == 50
+    
+    mock_get_estatisticas.assert_called_once_with()
